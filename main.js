fa18af79d61c34f26cc6aa78203cca2e90fb3e59
Franco <francodegano@hotmail.com>
2018-08-22 18:28:04 -0300

    index y main

---
+++ main.js
@@ -0,0 +1,18 @@
+var variableString= "hola mundo";
+var variableNumero = 22;
+var variableArray= [1,2,3];
+
+var variableObjeto = {
+    id:22,
+    nombre: "Alexis",
+    arrayNumeros:[
+        2,2,2
+    ]
+
+
+}
+for(var i=0;i<variableArray.length;i++)
+{
+    console.log(variableArray[i]);
+}
+
