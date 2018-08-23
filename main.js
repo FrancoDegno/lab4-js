4cb18c2f75acc4b453540785a8b5910c318895b8
franco <francodegano@hotmail.com>
2018-08-22 23:40:28 -0300

    commit Calculadora

--- main.js
+++ main.js
@@ -1,38 +1,103 @@
 
 var num1,num2;
+var op1,op2;
 
 function sum(nro1,nro2)
 {
-    return (Number(nro1)+Number(nro2)).toString();
+
+        if(Number(nro1)!=null && Number(nro2)!=null)
+            return Number(nro1)+Number(nro2);
+        else
+            return SyntaxError;
 }
 
 function mult(nro1,nro2)
 {
-    return (Number(nro1)*Number(nro2)).toString();
+    return Number(nro1)*Number(nro2);
 }
 
 function div(nro1,nro2)
 {
-    return (Number(nro1)/Number(nro2)).toString();
+    if(Number(nro2==0))
+    return "SyntaxError";
+    else
+    return Number(nro1)/Number(nro2);
 }
 
 function rest(nro1,nro2)
 {
-    return (Number(nro1)-Number(nro2)).toString();
+    return Number(nro1)-Number(nro2);
 }
 
 function captureN(butinp)
 {
-    document.getElementById("textid").innerText+=butinp;
+    console.log(document.getElementById("textid").length);
+    if(Number(document.getElementById("textid").innerText.length)<15)
+        document.getElementById("textid").innerText+=butinp;
 }
 
 
+function result()
+{
+    var index1=document.getElementById("textid").innerText.indexOf(op1);
+    var index2=document.getElementById("textid").innerText.length;
+    
+    num2=document.getElementById("textid").innerText.substring(index1+1,index2);
+    switch(op1)
+    {   
+        case "+":
+        document.getElementById("textid").innerText=sum(num1,num2);
+        op1=null;
+        break;
+        case "-":
+        document.getElementById("textid").innerText=rest(num1,num2);
+        op1=null;
+        break;
+        case "*":
+        document.getElementById("textid").innerText=mult(num1,num2);
+        op1=null;
+        break;
+        case "/":
+        document.getElementById("textid").innerText=div(num1,num2);
+        op1=null;
+        break;
 
-function captureOp()
+    }
+
+}
+
+function clear()
+{
+    document.getElementById("textid").innerText="";
+    num1=0;
+    num2=0;
+    op1=null;
+}
+
+
+function captureOp(op)
 {
 
+    if(op=="C")
+    {
+        clear();
+        return;
+    }
+    
+
+    if(op1==null){
+        num1=document.getElementById("textid").innerText;
+        op1=op;
+    }   
+    else{
+        result();
+        num1=document.getElementById("textid").innerText;
+        op1=op;
+    }
+    document.getElementById("textid").innerText+=op;
+
 }
 
-document.getElementById("1id").addEventListener("click",capture1,false);
+
 
 
